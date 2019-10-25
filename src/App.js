e1da445047b0da98c98f3cee1016de3c48615f8c
Vishal prajapati <vb7634@gmail.com>
2019-10-24 23:26:01 -0400

    fix the number

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <div> Hellow React 1 </div>
+          <div> Hellow React 2 </div>
       </header>
     </div>
   );
